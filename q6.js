ed9c20d867d5a91d887c025ffb4d77687801ccec
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-08-24 16:38:50 +0100

    Finished q6

--- q6.js
+++ q6.js
@@ -6,6 +6,31 @@
  * @return {number}
  */
 exports.q6 = function () {
-
-    return 0;
+    const size = 20;
+    let two_dim_array = twoDimArray(size, size);
+    two_dim_array[0][0] = 1;
+    for (let i = 0; i <= size; i++) {
+        for (let j = 0; j <= size; j++) {
+            if (i) {
+                two_dim_array[i][j] += two_dim_array[i - 1][j];
+            }
+            if (j) {
+                two_dim_array[i][j] += two_dim_array[i][j - 1];
+            }
+        }
+    }
+    return two_dim_array[size][size];
 };
+
+/**
+ * Makes a two-dimensional array sized: m x n
+ *
+ * @return {any[]}
+ */
+function twoDimArray(m, n) {
+    let dp = new Array(m + 1);
+    for (let i = 0; i <= m; i++) {
+        dp[i] = new Array(n + 1).fill(0);
+    }
+    return dp;
+}
