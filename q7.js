435b99a574afb3c62bed27e51bf589ca4d7833cf
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-08-24 16:06:33 +0100

    Added block comments to q5 and q6, as well as added triangle.txt

--- q7.js
+++ q7.js
@@ -1,3 +1,17 @@
+/**
+ * By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+ *
+ * (3)
+ * (7) 4
+ * 2 (4) 6
+ * 8 5 (9) 3
+ *
+ * That is, 3 + 7 + 4 + 9 = 23.
+ *
+ * Find the maximum total from top to bottom in triangle.txt (attached to the email), a 15K text file containing a triangle with one-hundred rows.
+ *
+ * @return {number}
+ */
 exports.q7 = function () {
     return 0;
 };
