9e371855952db70f2888ae4ef075f6cb331522fb
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-08-24 15:42:42 +0100

    Finished q5

--- q5.js
+++ q5.js
@@ -1,3 +1,37 @@
+/**
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+ *
+ * What is the 10,001st prime number?
+ *
+ * @return {number}
+ */
 exports.q5 = function () {
-    return 0;
+    let num_of_primes = 0;
+    let current_num = 1;
+    while (num_of_primes < 10001) {
+        current_num++;
+        if (isPrime(current_num)) {
+            num_of_primes++;
+        }
+    }
+    return current_num;
 };
+
+/**
+ * Is number p a prime number
+ *
+ * @return {boolean}
+ */
+function isPrime(p) {
+    if (p < 2) {
+        return false;
+    } else if (p === 2) {
+        return true;
+    }
+    for (let i = 2; i < Math.sqrt(p) + 1; i++) {
+        if (p % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
