93338e06ea79ab7b7107e25bd82ef0bdca27236c
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-08-24 14:05:19 +0100

    Finished q1

--- q1.js
+++ q1.js
@@ -1,3 +1,24 @@
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ * @returns {number}
+ */
 exports.q1 = function () {
-    return 0;
+    let total = 0;
+    for (let i = 0; i < 1000; i++) {
+        if (divisibleBy(i, 3) || divisibleBy(i, 5)) {
+            total += i;
+        }
+    }
+    return total;
 };
+
+/**
+ * Is number a divisible by number b.
+ * @return {boolean}
+ */
+function divisibleBy(a, b) {
+    return a % b === 0;
+}
